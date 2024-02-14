62a95045e66d88b883db1246b43ef477cce5c304
Alejandro Rios <alejandro.rios.cyt@gmail.com>
2024-02-14 19:21:23 +0100

    first commit

---
+++ src/components/Button.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import "../stylesheets/Button.css";
+
+function Button(props) {
+  const isOperator = (valor) => {
+    return isNaN(valor) && valor !== "." && valor !== "=";
+  };
+
+  return (
+    <div
+      className={`button-container ${
+        isOperator(props.children) ? "operator" : ""
+      }`.trimEnd()}
+      onClick={() => props.manageClick(props.children)}
+    >
+      {props.children}
+    </div>
+  );
+}
+
+export default Button;
